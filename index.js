8a42501a4f884f58249add9ae1d86019f5c83602
Hyoyeol Maeng <hyoyeol.maeng@geappliances.com>
2023-12-07 13:53:31 +0900

    the real first commit

--- index.js
+++ index.js
@@ -14,127 +14,160 @@ const port = 3000;
 
 const table_name = 'laundry_items'
 const column_id = 'id';
-const column_info = 'info';
+const column_temp = 'temperature';
+const column_spin = 'spin';
+const column_count = 'count';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let db = new sqlite3.Database('test_db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    } else {
-        console.log('gggggg');
-        db.run(`CREATE TABLE ${table_name}( \
+   if (err) {
+      return console.error(err.message);
+   } else {
+      console.log('creating table');
+      db.run(`CREATE TABLE ${table_name}( \
             ${column_id} INTEGER PRIMARY KEY NOT NULL, \
-            ${column_info} INTEGER NOT NULL \
+            ${column_temp} INTEGER NOT NULL, \
+            ${column_spin} INTEGER NOT NULL, \
+            ${column_count} INTEGER NOT NULL
             )`, (err) => {
-            if (err) {
-                console.log('Table already exists');
-            }
-        });
-    }
-    console.log('Connected to the in-memory SQLite DB');
+         if (err) {
+            console.log('Table already exists' + err.message);
+         }
+      });
+   }
+   console.log('Connected to the in-memory SQLite DB');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+   res.send('Up and Running!');
+});
 
-//curl -d "id=1&info=10" -X POST http://localhost:3000/add/
+//curl -d "id=1&temp=1&spin=1" -X POST http://localhost:3000/add/
 app.post('/add/', (req, res, next) => {
-    let data = {
-        id: req.body.id,
-        info: req.body.info
-    }
-    let sql = `INSERT INTO ${table_name} (${column_id}, ${column_info}) VALUES (?,?)`;
-    let params = [data.id, data.info];
+   let data = {
+      id: req.body.id,
+      temp: req.body.temp,
+      spin: req.body.spin
+   }
+   let sql = `INSERT INTO ${table_name} (${column_id}, ${column_temp}, ${column_spin}, ${column_count}) VALUES (?,?,?,?)`;
+   let params = [data.id, data.temp, data.spin, 1];
 
-    db.run(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
-        }
-        res.json({
-            "message": "success",
-            "id": data.id,
-            "info": data.info
-        });
-    });
+   db.run(sql, params, (err, result) => {
+      if (err) {
+         res.status(400).json({ "error": err.message })
+         return;
+      }
+      res.json({
+         'result': 'success',
+         'data': {
+            'id': data.id,
+            'temp': data.temp,
+            'spin': data.spin,
+            'count': 1
+         }
+      });
+   });
 });
 
-// curl -d "id=2&info=250" -X PATCH http://localhost:3000/update/
-app.patch("/update/", (req, res, next) => {
-    let data = {
-        id: req.body.id,
-        info: req.body.info
-    }
-    let sql = `UPDATE ${table_name} set ${column_info} = ? WHERE ${column_id} = ?`;
-    let params = [data.info, data.id];
+// // curl -d "id=2&temp=250" -X PATCH http://localhost:3000/update/
+// app.patch("/update/", (req, res, next) => {
+//    let data = {
+//       id: req.body.id,
+//       temp: req.body.temp
+//    }
+//    let sql = `UPDATE ${table_name} set ${column_temp} = ? WHERE ${column_id} = ?`;
+//    let params = [data.temp, data.id];
 
-    db.run(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ "error": res.message })
-            return;
-        }
-        res.json({
-            'message': "success",
-            'data': data,
-            'changes': this.changes
-        });
-    });
+//    db.run(sql, params, (err, result) => {
+//       if (err) {
+//          res.status(400).json({ "error": res.message })
+//          return;
+//       }
+//       res.json({
+//          'result': "success",
+//          'data': data,
+//          'changes': this.changes
+//       });
+//    });
+// });
+
+// curl -d "id=2&count=2" -X PATCH http://localhost:3000/increase_count/
+app.patch("/increase_count/", (req, res, next) => {
+   let data = {
+      id: req.body.id,
+      count: req.body.count
+   }
+   let sql = `UPDATE ${table_name} set ${column_count} = ? WHERE ${column_id} = ?`;
+   let params = [data.count, data.id];
+
+   db.run(sql, params, (err, result) => {
+      if (err) {
+         res.status(400).json({ "error": res.message })
+         return;
+      }
+      res.json({
+         'result': "success",
+         'data': data,
+         'changes': this.changes
+      });
+   });
 });
+
+
 
 // http://localhost:3000/all_items/
 app.get('/all_items/', (req, res, next) => {
-    let sql = `SELECT * from ${table_name}`
-    let params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(400).json({ 'error': err.message });
-            return;
-        }
-        res.json({
-            'message': 'success',
-            'data': rows
-        });
-    });
+   let sql = `SELECT * from ${table_name}`
+   let params = []
+   db.all(sql, params, (err, rows) => {
+      if (err) {
+         res.status(400).json({ 'error': err.message });
+         return;
+      }
+      res.json({
+         'result': 'success',
+         'data': rows
+      });
+   });
 });
 
 // http://localhost:3000/item/2
 app.get('/item/:id', (req, res, next) => {
-    let sql = `SELECT * from ${table_name} WHERE id = ?`
-    let params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ 'error': err.message });
-            return;
-        }
-        res.json({
-            'message': 'success',
-            'data': row
-        });
-    });
+   let sql = `SELECT * from ${table_name} WHERE id = ?`
+   let params = [req.params.id];
+   db.get(sql, params, (err, row) => {
+      if (err) {
+         res.status(400).json({ 'error': err.message });
+         return;
+      }
+      res.json({
+         'result': 'success',
+         'data': row
+      });
+   });
 });
 
 let server = app.listen(port, () => {
-    console.log(`Example listening on port ${port}`)
+   console.log(`Example listening on port ${port}`)
 })
 
 process.on('SIGINT', () => {
-    console.log('Closing DB Connection.');
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Close the DB connection');
-    });
+   console.log('Closing DB Connection.');
+   db.close((err) => {
+      if (err) {
+         return console.error(err.message);
+      }
+      console.log('Close the DB connection');
+   });
 
-    server.close((err) => {
-        console.log('Server killed');
-        process.exit(err ? 1 : 0);
-    });
+   server.close((err) => {
+      console.log('Server killed');
+      process.exit(err ? 1 : 0);
+   });
 });
 
 app.use((_, res) => {
-    res.status(404);
+   res.status(404);
 })
